0fdb411f79a5e89595ea56aa432eb928f3fd8e73
ThePandoConnection <lewiso.3101@gmail.com>
2023-04-18 23:29:12 +0100

    connection works successfully, just need to create code to read gcode now, will figure stuff out as we go along

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import serial
 import time
 
-ser = serial.Serial(port = "COM3", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-#f = open("demofile.gcode", "r")
-#print(f.read())
+def printerConnect(port='COM3', baudrate=115200):
+    ser = serial.Serial(port=port, baudrate=baudrate, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    print(ser.name + " is open")
+    return ser
 
-time.sleep(2)
 
-ser.write(str.encode('M106 S200'))
+def loadGcode(file):
+    f = open(file + '.gcode')
+    return f
 
-time.sleep(1)
 
-ser.close()
+def runPrinter(f):
+    return 0
+
+
+if __name__ == "__main__":
+    ser = printerConnect()
+    time.sleep(2)
+
+    ser.write(('G28\n').encode('ascii'))
+
+    time.sleep(1)
+
+    ser.close()
