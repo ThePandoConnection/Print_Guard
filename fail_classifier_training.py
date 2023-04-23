25001f6bc91474c45a15ce022c64c4e7f4f7b628
ThePandoConnection <lewiso.3101@gmail.com>
2023-04-23 15:25:10 +0100

    got image classifier working, needs a lot more training data but is a valid proof of concept.

--- fail_classifier_training.py
+++ fail_classifier_training.py
@@ -1,29 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
+import pathlib
 
-from tensorflow import keras
-from keras import layers
-from keras.models import Sequential
 
 # This Keras model is based on the Tensorflow tutorial: https://www.tensorflow.org/tutorials/images/classification#a_basic_keras_model
 # It is designed as a proof of concept and only the image collection and testing is truly original.
 
-import pathlib
-dataset_url = "https://git.cardiff.ac.uk/c1949524/cm2306_coursework/-/blob/Oliver's_Branch/Images.zip"
-data_dir = tf.keras.utils.get_file('print_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
 
 batch_size = 32
 img_height = 180
 img_width = 180
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
+    "./Images",
     validation_split=0.2,
     subset="training",
     seed=123,
@@ -31,7 +22,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=batch_size)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
+  "./Images",
   validation_split=0.2,
   subset="validation",
   seed=123,
@@ -41,7 +32,7 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
 class_names = train_ds.class_names
 print(class_names)
 
-normalization_layer = layers.Rescaling(1./255)
+normalization_layer = tf.keras.layers.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
@@ -51,17 +42,17 @@ print(np.min(first_image), np.max(first_image))
 
 num_classes = len(class_names)
 
-model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
@@ -74,6 +65,7 @@ history = model.fit(
   validation_data=val_ds,
   epochs=epochs
 )
+
 
 img = tf.keras.utils.load_img(
     #sunflower_path, target_size=(img_height, img_width)
