f87d7a70c8a292541b54a58234cdc1c6883dd453
Md Md Farid <MdFaridMA@cardiff.ac.uk>
2023-05-06 13:06:36 +0100

    Security modification by Md

--- project/Models.py
+++ project/Models.py
@@ -1,14 +1,29 @@
 from project import db, login_manager
+from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(UserMixin,db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id=db.Column(db.Integer,primary_key=True, autoincrement=True)
-    username=db.Column(db.String(15),unique=True,nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable.')
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
     def __repr__(self):
-        return f"User('{self.username}')"
+        return f"user('{self.username}')"
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
